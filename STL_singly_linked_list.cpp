ef8828dd3fce66620b9b0b10aaf079b1053d67a0
hyemsnail <163375128+hyemsnail@users.noreply.github.com>
2024-05-21 00:24:10 +0900

    Create STL_singly_linked_list.cpp

---
+++ STL_singly_linked_list.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+class Node { 
+public:
+	T data; // T타입의 데이터를 저장하는 멤버 변수 data 
+	Node<T>* next = nullptr; // 다음 노드를 가리키는 포인터 next 정의 , 마지막 노드의 포인터는 nullptr 
+
+};
+template<class T>
+class singleLinkedList { //리스트의 구성
+public:
+	singleLinkedList();
+	~singleLinkedList();
+	void push(T element); //element를 리스트의 마지막 원소로 넣는 함수
+	void show(); //리스트의 모든 원소값을 출력하는 함수
+
+private:
+	Node<T>* head; //리스트의 첫 번째 원소
+	Node<T>* tail; //리스트의 마지막 원소
+};
+template<class T>
+singleLinkedList<T>::singleLinkedList() : head(nullptr),tail(nullptr){} 
+
+template<class T>
+singleLinkedList<T>::~singleLinkedList() {
+	Node<T>* now_number = head;
+	while (now_number != nullptr) { // 마지막 노드의 다음이 nullptr이니 그것이 아니라면...
+		Node<T>* next = now_number->next;//다음 노드를 가리키는 포인터를 now_number->next로 설정
+		delete now_number; // 원래꺼는 메모리에서 해제해줘야 한다...
+		now_number = next; // 포인터를 그 다음 노드로 이동시킴 
+	}
+}
+template<class T>
+void singleLinkedList<T>::push(T element) {
+	Node<T>* newnode = new Node<T>(element);// 새로운 노드 생성, value값은 element로 초기화
+	if (tail == nullptr) { // 리스트가 아예 비어있는 경우
+		head = tail = newnode; //head와 tail을 새로운 노드로 설정
+
+	}
+	else { // 리스트가 비어있지 않는 경우
+		tail->next = newnode; //tail의 다음 노드를 새로운 노드로 설정한다.
+		tail = newnode; // tail을 새로운 노드로 이동시킴 
+	}
+
+
+}
+template<class T>
+void singleLinkedList<T>::show() {
+	Node<T>* now_number = head;
+	while (now_number != nullptr) {
+		cout << now_number->data;
+		now_number = now_number->next;
+	}
+}
+
+int main() {
+	singleLinkedList<int>* idx = new singleLinkedList<int>;
+	idx->push(1);
+	idx->push(2);
+	idx->push(3);
+	idx->push(4);
+	idx->push(5);
+	idx->show();
+	
+}
