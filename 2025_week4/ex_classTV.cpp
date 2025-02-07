9966bdc353634052c296e7858f0caa9f9264fdb3
hyemsnail <163375128+hyemsnail@users.noreply.github.com>
2025-02-08 00:20:43 +0900

    Create ex_classTV.cpp

---
+++ 2025_week4/ex_classTV.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class TV {
+	bool on;
+	int channel;
+	int volume;
+public:
+	void poweron() {}
+	void poweroff() {}
+	void channelncrease() {}
+	void channelDecrease() {}
+	void volumeIncrease() {}
+	void volumeDecrease() {}
+};
+
+int main() {
+
+}
