ae91606cee23fd6a882c3c890f3cd9f8a4352105
hyemsnail <163375128+hyemsnail@users.noreply.github.com>
2025-02-08 00:22:19 +0900

    Create delegating_calculator.cpp

---
+++ 2025_week4/delegating_calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+class Rectangle { // Rectangle 클래스 선언
+public:
+	int width; //멤버 변수
+	int height; //멤버 변수
+	int area(); //멤버 함수
+	Rectangle();
+	Rectangle(int w, int h);
+};
+
+Rectangle::Rectangle() : Rectangle(6, 10) {} //위임 생성자
+
+Rectangle::Rectangle(int w, int h) { //타겟 생성자
+	width = w;
+	height = h;
+}
+
+int Rectangle::area() {
+	return width * height;
+}
+
+int main() {
+	Rectangle rect1;
+	cout << "직사각형1의 면적은 " << rect1.area() << endl;
+	Rectangle rect2(3, 7);
+	cout << "직사각형2의 면적은 " << rect2.area() << endl;
+}
